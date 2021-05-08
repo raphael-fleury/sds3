4cefb2c768f1ac9332f47cf65f1bc7908d4763c6
Raphael Fleury <raphael.fleury9@gmail.com>
2021-05-08 12:12:41 -0300

    frontend: DataTable done and getting API path from env variable

--- frontend/src/utils/requests.ts
+++ frontend/src/utils/requests.ts
@@ -1,2 +1,1 @@
-// export const BASE_URL = 'http://localhost:8080';
-export const BASE_URL = 'https://sds3-raphael-fleury.herokuapp.com';
+export const BASE_URL = process.env.REACT_APP_API_PATH ?? 'http://localhost:8080';
